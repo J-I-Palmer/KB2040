c9885d1eec9ed71ea8f1563edddfcb3056292661
James Palmer <jpalmer8@una.edu>
2024-09-19 12:00:29 -0500

    Added new button game program

---
+++ pio/ws2811/ws2811_button_game.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "ws2811.pio.h"
+
+int main(){
+
+	const uint LED_PIN = 3;
+	const int NUM_LEDS = 50;
+	const uint BUTTON_PIN = 10;
+
+	PIO pio = pio0;
+	uint sm = 0;
+
+	uint offset = pio_add_program(pio, &ws2811_program);
+	ws2811_program_init(pio, sm, offset, LED_PIN, 800000, 0);
+
+	uint32_t pixel;
+	bool pressed;
+	bool reset;
+
+	gpio_init(BUTTON_PIN);
+	gpio_set_dir(BUTTON_PIN, GPIO_IN);
+
+	while(true){
+		reset = false;
+		pixel = 0x00000000;
+
+		for(int i = 0; i < NUM_LEDS; i++)
+			pio_sm_put_blocking(pio, sm, pixel);
+		sleep_ms(500);
+
+		pixel = 0x0000ff00; // RRGGBB--
+		for(int i = NUM_LEDS; i > 0 && pressed == false; i--){
+			for(int j = 0; j < NUM_LEDS; j++)
+				pio_sm_put_blocking(pio, sm, pixel);
+			sleep_ms(40);
+			if (gpio_get(BUTTON_PIN)){
+				sleep_ms(1);
+				pressed = gpio_get(BUTTON_PIN);
+			}
+		}
+
+		if(!pressed){
+			pixel = 0xff000000;
+			for(int i = 0; i < NUM_LEDS; i++)
+				pio_sm_put_blocking(pio, sm, pixel);
+		}
+
+		while(!reset){
+			sleep_ms(100);
+			if(gpio_get(BUTTON_PIN)){
+				sleep_ms(1);
+				reset = gpio_get(BUTTON_PIN);
+			}
+		}
+	}
+
+	return 0;
+}
